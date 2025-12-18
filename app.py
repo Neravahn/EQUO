e4794504adce598b104cb93d20fb2d036e1687b7
Neravahn <prashantc48774.r@gmail.com>
2025-12-18 18:13:12 +0530

    Added fire base login, now i have to add the backend for teh file storage

--- app.py
+++ app.py
@@ -1,14 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+import firebase_admin
+from firebase_admin import credentials, auth
 
 
 app = Flask(__name__)
 
+cred = credentials.Certificate("firebase_key.json")
+firebase_admin.initialize_app(cred)
+
 
 @app.route('/', methods = ['GET', 'POST'])
 def index():
-
-
     return render_template('index.html')
+
+@app.route('/auth/login', methods=['POST'])
+def auth_login():
+    try:
+        data = request.get_json()
+        token = data.get("token")
+
+        decoded = auth.verify_id_token(token)
+        return jsonify({"success": True})
+
+    except Exception as e:
+        print("AUTH ERROR:", e)
+        return jsonify({"success": False, "error": str(e)}), 401
+
+@app.route('/dashboard')
+def dashboard():
+    return "Logged in successfully 🎉"
+
+
 
 
 
